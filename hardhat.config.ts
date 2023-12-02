e28ea48f6423d4197088138e4dfa9966a3bfad74
rookiecol <juan.diegoam@hotmail.com>
2023-12-02 16:15:08 -0500

    :construction: Mock nft

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,22 @@
+// Import necessary modules
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from 'dotenv';
+
+// Load environment variables from the .env file
 dotenv.config();
 
+// Check if PRIVATE_KEY is set in the .env file
 if (!process.env.PRIVATE_KEY) {
   throw new Error("PRIVATE_KEY is not defined in .env file");
 }
 
+// Check if ETHERSCAN_API_KEY is set in the .env file
+if (!process.env.ETHERSCAN_CELO_API_KEY) {
+  throw new Error("ETHERSCAN_API_KEY is not defined in .env file");
+}
+
+// Hardhat configuration
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
@@ -16,7 +26,22 @@ const config: HardhatUserConfig = {
       accounts: [process.env.PRIVATE_KEY]
     }
   },
-defaultNetwork: "alfajores"
+  etherscan: {
+    apiKey: {
+      alfajores: process.env.ETHERSCAN_CELO_API_KEY
+    },
+    customChains: [
+      {
+        network: "alfajores",
+        chainId: 44787,
+        urls: {
+          apiURL: "https://api-alfajores.celoscan.io/api",
+          browserURL: "https://alfajores.celoscan.io",
+        }
+      }
+    ]
+  },
+  defaultNetwork: "alfajores"
 };
 
 export default config;
